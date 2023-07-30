8cdbe3c7c9802613714487d6889a98be6a1cdae6
pinto541 <78383405+pinto541@users.noreply.github.com>
2023-07-31 03:07:47 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+const { requireSignin, isAuth, isAdmin } = require('./controllers/auth');
 
 const {
   userById,
   read,
   update,
   purchaseHistory,
-} = require('../controllers/user');
+} = require('./controllers/user');
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
   res.json({
