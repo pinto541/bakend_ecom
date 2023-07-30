d41fefe16556626b0dde017b34bff3540fb40e08
pinto541 <78383405+pinto541@users.noreply.github.com>
2023-07-31 03:17:06 +0530

    Update order.js

--- routes/order.js
+++ routes/order.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const { requireSignin, isAuth, isAdmin } = require('./controllers/auth');
-const { userById, addOrderToUserHistory } = require('./controllers/user');
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+const { userById, addOrderToUserHistory } = require('../controllers/user');
 const {
   create,
   listOrders,
   getStatusValues,
   orderById,
   updateOrderStatus,
-} = require('./controllers/order');
+} = require('../controllers/order');
 
-const { decreaseQuantity } = require('./controllers/product');
+const { decreaseQuantity } = require('../controllers/product');
 
 router.post(
   '/order/create/:userId',
