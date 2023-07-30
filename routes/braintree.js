17a87fae03b5fa329d4e40dcd9cc1a1aeaffc5ed
pinto541 <78383405+pinto541@users.noreply.github.com>
2023-07-31 03:04:46 +0530

    Update braintree.js

--- routes/braintree.js
+++ routes/braintree.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { requireSignin, isAuth } = require('../controllers/auth');
-const { userById } = require('../controllers/user');
-const { generateToken, processPayment } = require('../controllers/braintree');
+const { requireSignin, isAuth } = require('./controllers/auth');
+const { userById } = require('./controllers/user');
+const { generateToken, processPayment } = require('./controllers/braintree');
 
 router.get('/braintree/getToken/:userId', requireSignin, isAuth, generateToken);
 router.post(
